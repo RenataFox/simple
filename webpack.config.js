53d4336de943774cbdc3ec8cfcb73c9d13b86216
RenataFox <renata_letters@mail.ru>
2019-04-29 10:29:16 +1100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    open:true,  
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      { 
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+      }
+    ]
+  },
+  plugins: [ 
+    new HtmlWebpackPlugin({
+        template: './src/index.pug'
+    }),
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};
